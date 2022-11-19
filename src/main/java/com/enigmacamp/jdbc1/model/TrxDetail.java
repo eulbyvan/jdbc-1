8755df7d1d478307df37f9a440536ca5eb8fce8a
eulbyvan <work.issdq@gmail.com>
2022-11-19 19:50:49 +0700

    add TrxDetail attributes

--- src/main/java/com/enigmacamp/jdbc1/model/TrxDetail.java
+++ src/main/java/com/enigmacamp/jdbc1/model/TrxDetail.java
@@ -5,4 +5,55 @@ public class TrxDetail {
     Integer trxId;
     Integer productId;
     Integer qty;
+
+    public TrxDetail() {}
+
+    public TrxDetail(Integer id, Integer trxId, Integer productId, Integer qty) {
+        this.id = id;
+        this.trxId = trxId;
+        this.productId = productId;
+        this.qty = qty;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getTrxId() {
+        return trxId;
+    }
+
+    public void setTrxId(Integer trxId) {
+        this.trxId = trxId;
+    }
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public Integer getQty() {
+        return qty;
+    }
+
+    public void setQty(Integer qty) {
+        this.qty = qty;
+    }
+
+    @Override
+    public String toString() {
+        return "TrxDetail{" +
+                "id=" + id +
+                ", trxId=" + trxId +
+                ", productId=" + productId +
+                ", qty=" + qty +
+                '}';
+    }
 }
